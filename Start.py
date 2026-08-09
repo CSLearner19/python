eace5d7b67466e5520c9e3f5e554c375afe115e5
CSLearner19 <K2309961@students.katyisd.org>
2026-08-09 08:13:54 -0500

    Add files via upload

---
+++ Start.py
@@ -0,0 +1,8 @@
+#print hello world
+print ("Hello World");
+#printing my name
+print ("My name is Tejasman");
+#printing fun fact
+print ("I love playing soccer");
+#performing simple calculator
+print ("The sum of 3+5:", 3+5);
