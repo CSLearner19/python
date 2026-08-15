8ca22466f5d7ee1a667ba01eb9ee0e0bc30181bb
CSLearner19 <K2309961@students.katyisd.org>
2026-08-15 08:21:55 -0500

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,11 @@
+print("Birthdays of 5 friends\n")
+a="13/02/92"
+print("aakash -", a)
+b="16/05/95"
+print("\nsunny-", b)
+c="06/05/92"
+print("\nsweety-",c)
+d="12/02/92"
+print("\npooja-",d)
+e="18/04/96"
+print("\nram-",e)
